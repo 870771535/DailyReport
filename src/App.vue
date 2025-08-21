2f53558b7c390c9c69dbc9ecb0e0131e63cc8984
zhanghang <zhanghang160@huawei-partners.com>
2025-08-21 17:22:58 +0800

    vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView class="wrapper"/>
+</template>
+
+<style scoped>
+.wrapper{
+  max-height: 100vh;
+  max-width: 100vw;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+const input = ref('')
+
+
+</script>
+
+<template>
+  <main>
+    <form autocomplete="off">
+      <el-input v-model="input" style="width: 240px" placeholder="Please input" />
+    </form>
+    <el-button type="primary">cherry pick</el-button>
+  </main>
+</template>
